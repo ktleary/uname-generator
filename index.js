87e23256f2d79188ab00d4eabb5e2e7d04fed738
ktleary <kevin@ktleary.com>
2021-01-25 00:26:18 +0800

    remove reassignments

--- index.js
+++ index.js
@@ -1,34 +1,39 @@
-const beautifulConsonants = 'bcdfghjklmnprst'
-const vowels = 'aeiou'
-const all = beautifulConsonants.concat(vowels)
+const beautifulConsonants = "bcdfghjklmnprstyw";
+const vowels = "aeiou";
+const all = beautifulConsonants.concat(vowels);
 
-const randomChar = string => string[Math.floor(Math.random() * string.length)]
+// randomChar :: String -> String
+const randomChar = (string) =>
+  string[Math.floor(Math.random() * string.length)];
 
+// generateUname :: Number -> String
 function generateUname(length = 8) {
-  if (length < 4) return false
-  return new Array(length).fill(null).reduce((result, _, idx) => {
-    /* eslint-disable no-param-reassign */
-    if (idx === 0) {
-      result = randomChar(all)
-    } else if (idx === 1 || idx === 2) {
-      result += randomChar(beautifulConsonants)
-    } else if (idx === 3) {
-      result += randomChar(vowels)
-    } else if (idx % 2 !== 0) {
-      result += randomChar(vowels)
-    } else {
-      result += randomChar(all)
-    }
-    return result
-  }, '')
+  if (length < 4) return false;
+  // switch character choice by letter position
+  return Array(length)
+    .fill(0)
+    .reduce((result, _, idx) => {
+      if (idx === 0) {
+        return result.concat(randomChar(all));
+      } else if (idx === 1 || idx === 2) {
+        return result.concat(randomChar(beautifulConsonants));
+      } else if (idx === 3) {
+        return result.concat(randomChar(vowels));
+      } else if (idx % 2 !== 0) {
+        return result.concat(randomChar(vowels));
+      }
+      return result.concat(randomChar(all));
+    }, "");
 }
 
 function main() {
-  const unameLength = 6
-  const unname = generateUname(unameLength)
-  process.stdout.write(unname.toString())
+  const writeUname = (uname) => console.log(uname);
+  const unameLength = 8;
+  const uname = generateUname(unameLength);
+  return writeUname(uname);
 }
 
-main()
+/* eslint-disable fp/no-unused-expression */
+main();
 
-module.exports = generateUname
+module.exports = generateUname;
